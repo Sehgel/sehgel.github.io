23e04d77e777c810cdee60c3a1b8d53869f6c82f
Sehgel <sehgel.dev@gmail.com>
2026-04-06 20:19:11 -0300

    Add localization first try

---
+++ js/locale.js
@@ -0,0 +1,33 @@
+let locale = "en";
+
+const translations = {
+    "es": {
+        "home": "Inicio",
+        "about": "Información",
+        "services": "Servicios",
+        "contact": "Contacto",
+        "websites": "Sitios",
+        "built": "construidos",
+        "differently": "diferente",
+        "locale": "es"
+    },
+};
+
+
+// When the page content is ready...
+document.addEventListener("DOMContentLoaded", () => {
+    if (locale in translations == false)
+        return;
+  document
+    // Find all elements that have the key attribute
+    .querySelectorAll("[data-i18n-key]")
+    .forEach(translateElement);
+});
+
+function translateElement(element) {
+  const key = element.getAttribute("data-i18n-key");
+  if (key in translations[locale] == false)
+    return;
+  const translation = translations[locale][key];
+  element.innerText = translation;
+}
